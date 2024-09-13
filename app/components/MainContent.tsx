469d313ab6861e974a68a0d8edf3eef04e2d98bb
petershaan12 <2181002@unai.edu>
2024-09-13 07:33:47 +0700

    update

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -3,6 +3,9 @@
 import { useState, useEffect, useRef } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoGlobeOutline } from "react-icons/io5";
+import Link from "next/link";
 
 const WeddingScreen = () => {
   const [fadeClass, setFadeClass] = useState("opacity-0"); // Untuk fade-in pertama kali
@@ -25,6 +28,36 @@ const WeddingScreen = () => {
       (audioRef.current as HTMLAudioElement).play();
     }
   };
+
+  const slideRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    console.log("halo semua ");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log("halo semua 2 ");
+        entries.forEach((entry) => {
+          console.log("entries", entry); // Lihat status entry untuk memastikan observer berfungsi
+          if (entry.isIntersecting) {
+            entry.target.classList.add("active"); // Menambahkan kelas 'active' saat elemen terlihat
+          } else {
+            entry.target.classList.remove("active"); // Menghapus kelas 'active' saat elemen tidak terlihat lagi (opsional)
+          }
+        });
+      },
+      { threshold: 0.5 } // Adjust as needed
+    );
+
+    slideRefs.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      slideRefs.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
 
   return (
     <div
@@ -82,16 +115,19 @@ const WeddingScreen = () => {
         </div>
         {isOpen && (
           <>
+            {/* Slide 1 */}
             <div
-              className=" text-white h-screen flex pt-12 p-5 "
+              ref={(el) => {
+                slideRefs.current[0] = el;
+              }}
+              className="  text-white h-screen flex pt-12 p-5 "
               style={{
                 backgroundImage: `url(/slide_1.jpg)`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
             >
-              {/* Display the content when the button is clicked */}
-              <div className="relative z-10">
+              <div className="relative z-10 fadeInMove">
                 <h1 className="text-xl md:text-2xl font-ovo tracking-wide text-white ">
                   ECCLESIASTES 4:9-12
                 </h1>
@@ -106,8 +142,12 @@ const WeddingScreen = () => {
                 <p className="text-6xl mt-5 font-wonder">Glenn & Cindy</p>
               </div>
             </div>
+            {/* Slide 2 */}
             <div
-              className=" text-white h-screen flex items-end pb-16 px-12 "
+              ref={(el) => {
+                slideRefs.current[1] = el;
+              }}
+              className=" text-white h-screen flex items-end pb-16 px-12"
               style={{
                 backgroundImage: `url(/slide_2.jpeg)`,
                 backgroundSize: "cover",
@@ -115,8 +155,8 @@ const WeddingScreen = () => {
               }}
             >
               {/* Display the content when the button is clicked */}
-              <div className="relative z-10">
-                <p className="font-legan text-sm my-2">The Groom</p>
+              <div className="fadeInMove">
+                <p className="fadeInMove font-legan text-sm my-2">The Groom</p>
                 <h1 className="text-xl md:text-3xl text-white  font-ovo">
                   GLENN REYSAN
                 </h1>
@@ -133,6 +173,7 @@ const WeddingScreen = () => {
                 </p>
               </div>
             </div>
+            {/* Slide 3 */}
             <div
               className=" text-white h-screen flex items-end pb-16 px-12 "
               style={{
@@ -160,6 +201,7 @@ const WeddingScreen = () => {
                 </p>
               </div>
             </div>
+            {/* Slide 4 */}
             <div
               className=" text-white h-screen flex  pt-16 px-12 "
               style={{
@@ -212,6 +254,7 @@ const WeddingScreen = () => {
                 </div>
               </div>
             </div>
+            {/* Slide 5 */}
             <div
               className=" text-white h-screen flex flex-col items-center pt-16 px-12 "
               style={{
@@ -252,6 +295,7 @@ const WeddingScreen = () => {
                 </p>
               </div>
             </div>
+            {/* Slide 6 */}
             <div
               className=" text-white h-screen flex flex-col items-center justify-end pb-16 px-12 "
               style={{
@@ -283,6 +327,7 @@ const WeddingScreen = () => {
                 </div>
               </div>
             </div>
+            {/* Slide 7 */}
             <div
               className=" text-white h-screen flex flex-col justify-between pt-16 pb-32 px-12 "
               style={{
@@ -309,6 +354,101 @@ const WeddingScreen = () => {
                   Join Live Streaming
                 </p>
               </div>
+            </div>
+            <div
+              className=" text-white h-screen flex flex-col justify-center pt-16 pb-16 px-8 "
+              style={{
+                backgroundImage: `url(/slide_8.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <h1 className="text-3xl text-white  font-ovo text-center uppercase">
+                Unveiling Our Prewedding Story
+              </h1>
+
+              <div className="mt-10 mx-auto w-full max-w-2xl">
+                <iframe
+                  width="100%"
+                  height="200"
+                  src="https://www.youtube.com/embed/A_vZszz89Ds?autoplay=1&mute=1"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                ></iframe>
+              </div>
+
+              <div className="-mt-12 w-72 transform skew-x-6 drop-shadow">
+                <p className="text-3xl font-thesignature text-white/80 ">
+                  Every love story is beautiful, but ours is my favorite.
+                  Through the highs and lows, our love grows stronger and deeper
+                  with each passing day
+                </p>
+              </div>
+            </div>
+            <div
+              className=" text-white h-screen flex flex-col justify-end pt-16 pb-16 px-12 "
+              style={{
+                backgroundImage: `url(/slide_7.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <h1 className="text-3xl text-white  font-ovo text-center">
+                THANK YOU FOR YOUR ATTENDANCE
+              </h1>
+
+              <div className="mt-5 mx-auto flex flex-col ">
+                <p className="text-sm font-legan text-white text-center">
+                  It is a pleasure and honor for us, if you are willing to
+                  attend and give us your blessing.
+                </p>
+                <p className="text-sm rounded-full text-center font-ovo mt-5 px-6 py-2 text-white">
+                  GLENN & CINDY
+                </p>
+              </div>
+
+              <footer className="flex flex-col items-center mt-8">
+                <p className="text-[0.5rem] uppercase text-center">
+                  Created By Peter Shaan
+                </p>
+                <div>
+                  <div className="flex gap-x-3">
+                    <Link
+                      href="https://wa.me/6289529882952"
+                      target="_blank"
+                      className="my-2 flex items-center justify-center gap-x-2 hover:underline"
+                    >
+                      <FaWhatsapp />
+                      <p className=" tracking-widest text-[0.6rem]">
+                        +6289529882952
+                      </p>
+                    </Link>
+                    <Link
+                      href="https://instagram.com/petershaan_"
+                      target="_blank"
+                      className="my-2 flex items-center justify-center gap-x-2 hover:underline"
+                    >
+                      <FaInstagram />
+                      <p className=" tracking-widest text-[0.6rem]">
+                        @petershaan_
+                      </p>
+                    </Link>
+                  </div>
+                  <Link
+                    href="https://petershaan.com"
+                    target="_blank"
+                    className="mb-4 flex items-center justify-center gap-x-2 -mt-1 hover:underline"
+                  >
+                    <IoGlobeOutline />
+                    <p className=" tracking-widest text-[0.6rem]">
+                      petershaan.com
+                    </p>
+                  </Link>
+                </div>
+                <p className="text-xs">© All rights reserved by petershaan</p>
+              </footer>
             </div>
           </>
         )}
